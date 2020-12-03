034f6d6dd97f14d551032b2a1daffa20625d6698
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2020-12-02 22:54:47 -0800

    Check For Ground and Fixed player Ground clipping at high Velocities

--- Assets/Scripts/Avatar/Player/CharacterPhysics.cs
+++ Assets/Scripts/Avatar/Player/CharacterPhysics.cs
@@ -1,29 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CharacterPhysics : MonoBehaviour
 {
     //ToDo's
     //Player 2D collider at high speeds Clipping (Y Velocity 60) must Fix
+    // Floor Composit collider is uneven and moves my character sround with zero input. (Must Fix)
     protected Rigidbody2D Character_RB;
+    [SerializeField]
     protected Vector2 ApplyMovment = new Vector2();
+    protected CapsuleCollider2D _collider2D;
 
     private void OnEnable()
     {
+        _collider2D = GetComponent<CapsuleCollider2D>();
+        if (!_collider2D) Debug.Log("Collider 2D Required on " + gameObject.name);
         Character_RB = GetComponent<Rigidbody2D>();
         if (!Character_RB) Debug.Log("RigidBody2D Required on" + gameObject.name);
     }
-
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Break();
+            Character_RB.velocity = new Vector2(0, -1000000f); //Testing High Speed Velocity Ground Clipping
+        }
         InputMovement();
     }
 
     private void FixedUpdate()
     {
-        Character_RB.position += ApplyMovment * Time.deltaTime;
+        Character_RB.position += ApplyMovment * Time.fixedDeltaTime;
     }
 
     protected virtual void InputMovement()
--- Assets/Scripts/Avatar/Player/PlayerInput.cs
+++ Assets/Scripts/Avatar/Player/PlayerInput.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class PlayerInput : CharacterPhysics
 {
+    //ToDo's
+    // RayCast A Shere instead of a line for Rock Glitches player jumps on rocks with no input
     [SerializeField]
     private float Speed = 1;
     [SerializeField]
     private float jumpForce = 10;
+    [SerializeField]
+    private LayerMask layerMasking;
     private float rb_DeltaY_Velocity = 0;
+    [SerializeField]
     private Vector2 Player_Input = new Vector2();
+    private Vector2 Collider2D_Offset;
+    [SerializeField]
+    private float PlayerHight_GroundCheck = 0;
+
+    private void Start()
+    {
+        Collider2D_Offset = _collider2D.size;
+        if(Collider2D_Offset == Vector2.zero)
+        {
+            StartCoroutine(ColliderCheck());
+        }
+    }
 
     protected override void InputMovement()
     {
         Player_Input.x = Input.GetAxis("Horizontal") * Speed;
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Player_Input.y == 0)
         {
-            Debug.Break();
             Player_Input.y = jumpForce;
         }
         else if (Player_Input.y > 0)
@@ -24,12 +40,53 @@ public class PlayerInput : CharacterPhysics
             Player_Input.y += rb_DeltaY_Velocity + Character_RB.velocity.y;
             rb_DeltaY_Velocity = Mathf.Abs(Character_RB.velocity.y);
 
-            if(Player_Input.y < 0)
+            if(Player_Input.y <= 0)
             {
                 rb_DeltaY_Velocity = 0;
                 Player_Input.y = 0;
             }
         }
+        GroundChecker();
         ApplyMovment = Player_Input;
+    }
+
+    private void GroundChecker()
+    {
+        if(Character_RB.velocity.y != 0) // need a range for uneven ground
+        {
+            RaycastHit2D hit2D;
+            if(Character_RB.velocity.y < 0)
+            {
+                Vector2 CharacterColliderOffset = transform.position;
+                CharacterColliderOffset.y -= 0.5f;
+                hit2D = Physics2D.Raycast(CharacterColliderOffset, Vector3.down, NextFramePosition(), layerMasking);
+                Debug.DrawRay(CharacterColliderOffset,Vector2.down * NextFramePosition(), Color.green); // Delete
+                if(hit2D.collider != null)
+                {
+                    Debug.Log(hit2D.collider.name);
+                    PlayerHight_GroundCheck = hit2D.distance;
+                    if(NextFramePosition()+0.01f >= PlayerHight_GroundCheck)
+                    {
+                        Debug.Log("Ground Triggered");
+                        Character_RB.velocity = Vector2.zero;
+                        transform.position = hit2D.point + new Vector2(0, Collider2D_Offset.y/2);
+                    }
+                }
+            }
+        }
+    }
+
+    private float NextFramePosition()
+    {
+        return Mathf.Abs(Character_RB.velocity.y) * Time.fixedDeltaTime;
+    }
+
+    private IEnumerator ColliderCheck()
+    {
+        yield return new WaitForEndOfFrame();
+        if(Collider2D_Offset == Vector2.zero)
+        {
+            Debug.Log("Coroutine Find Collider Failed" + transform.name);
+        }
     }
 }
