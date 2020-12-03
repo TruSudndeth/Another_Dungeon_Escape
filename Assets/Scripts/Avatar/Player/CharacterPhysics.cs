41f49f3e11240fd9bcbf3ec1eb6c1bba9a0fcca8
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2020-12-02 19:12:01 -0800

    Player SetUp - Sprite Slicing Fixed to center (Bad Animation)

---
+++ Assets/Scripts/Avatar/Player/CharacterPhysics.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPhysics : MonoBehaviour
+{
+    //ToDo's
+    //Player 2D collider at high speeds Clipping (Y Velocity 60) must Fix
+    protected Rigidbody2D Character_RB;
+    protected Vector2 ApplyMovment = new Vector2();
+
+    private void OnEnable()
+    {
+        Character_RB = GetComponent<Rigidbody2D>();
+        if (!Character_RB) Debug.Log("RigidBody2D Required on" + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        Character_RB.position += ApplyMovment * Time.deltaTime;
+    }
+
+    protected virtual void InputMovement()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Avatar/Player/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : CharacterPhysics
+{
+    [SerializeField]
+    private float Speed = 1;
+    [SerializeField]
+    private float jumpForce = 10;
+    private float rb_DeltaY_Velocity = 0;
+    private Vector2 Player_Input = new Vector2();
+
+    protected override void InputMovement()
+    {
+        Player_Input.x = Input.GetAxis("Horizontal") * Speed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Break();
+            Player_Input.y = jumpForce;
+        }
+        else if (Player_Input.y > 0)
+        {
+            Player_Input.y += rb_DeltaY_Velocity + Character_RB.velocity.y;
+            rb_DeltaY_Velocity = Mathf.Abs(Character_RB.velocity.y);
+
+            if(Player_Input.y < 0)
+            {
+                rb_DeltaY_Velocity = 0;
+                Player_Input.y = 0;
+            }
+        }
+        ApplyMovment = Player_Input;
+    }
+}
