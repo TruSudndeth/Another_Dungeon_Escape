139e20155ca53433fea21b5c773ce5cf8d5f5636
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2020-12-11 19:28:20 -0800

    1. Added Moss_Giant animation Logic 2. added Sprite flip animation

---
+++ Assets/Scripts/Avatar/Enemy/AnimationBehaviours/Moss_Giant_Hit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moss_Giant_Hit : StateMachineBehaviour
+{
+    private bool OneShot = false;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        OneShot = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.normalizedTime > 0.1f && !OneShot)
+        {
+            OneShot = true;
+            animator.SetBool("Hit", false);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- Assets/Scripts/Avatar/Enemy/Moss_Giant.cs
+++ Assets/Scripts/Avatar/Enemy/Moss_Giant.cs
@@ -6,38 +6,57 @@ public class Moss_Giant : Enemy, IEnemy
 {
     [SerializeField]
     private MoveAnB PointA2B;
+    private Animator Anim;
     private Vector2 ApplyAIMove = Vector2.zero;
     private Rigidbody2D RB;
+    private bool isDead = false;
+    private bool IsMoving = false;
+    private SpriteRenderer SpriteRend;
     void Awake()
     {
+        SpriteRend = GetComponentInChildren<SpriteRenderer>();
+        Anim = GetComponentInChildren<Animator>();
         RB = GetComponent<Rigidbody2D>();
         if (PointA2B != null) PointA2B.FarthestPoint(transform);
+        StartCoroutine(RandomScout());
     }
     public override void Attack()
     {
-        throw new System.NotImplementedException();
+        Anim.SetBool("Attack", true);
     }
 
     public override void Update()
     {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage();
+        }
         CalcMoves();
     }
 
     public void FixedUpdate()
     {
+        if(isDead)
+        {
+            if (!Anim.GetBool("IsDeat"))
+            {
+                isDead = false;
+                StartCoroutine(IsDead());
+            }
+        }
         ApplyAIMove.x *= Speed;
+        FlipX();
         RB.velocity = new Vector2(ApplyAIMove.x, RB.velocity.y);
     }
 
     private void CalcMoves()
     {
-        if(PointA2B != null)
+        if(PointA2B != null && IsMoving)
         {
             ApplyAIMove = PointA2B.MoveToPoint(transform.position);
         }
         else
             ApplyAIMove = Vector2.zero;
-        
     }
 
     public void TakeDamage()
@@ -45,7 +64,39 @@ public class Moss_Giant : Enemy, IEnemy
         health--;
         if(health <= 0)
         {
-            Debug.Log("Enemy Killed");
+            IsMoving = false;
+            Anim.SetBool("IsDead", true);
+            isDead = true;
+        }
+    }
+
+    private IEnumerator IsDead()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator RandomScout()
+    {
+        while(!isDead)
+        {
+            IsMoving = false;
+            Anim.SetBool("Walking", false);
+            yield return new WaitForSeconds(Random.Range(2, 5f));
+            IsMoving = true;
+            Anim.SetBool("Walking", true);
+            yield return new WaitForSeconds(Random.Range(5, 7));
+        }
+    }
+    private void FlipX()
+    {
+        if (ApplyAIMove.x > 0)
+        { 
+            if (SpriteRend.flipX) SpriteRend.flipX = false;
+        }
+        if(ApplyAIMove.x < 0)
+        {
+            if (!SpriteRend.flipX) SpriteRend.flipX = true;
         }
     }
 }
